dc00b12bbeda4deac683b4f5b46df922aeb8eedd
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-03-14 16:41:19 -0700

    New Core app, all the functional core of the app will be there

---
+++ config/test.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+# Configure your database
+config :core, Core.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "core_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
