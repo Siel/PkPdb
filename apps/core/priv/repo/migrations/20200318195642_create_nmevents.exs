05d42bdcc9c0837882dd9eb238a54bc5f900ef41
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-03-18 13:09:52 -0700

    create a migration for NM

---
+++ apps/core/priv/repo/migrations/20200318195642_create_nmevents.exs
@@ -0,0 +1,25 @@
+defmodule Core.Repo.Migrations.CreateNmevents do
+  use Ecto.Migration
+
+  def change do
+    create table(:nm_events, primary_key: false) do
+      add(:id, :binary_id, primary_key: true)
+      # NONMEM's ID was changed to subject
+      add(:subject, :integer, null: false)
+      add(:time, :float, null: false)
+      # This is because they can use "." as a placeholder
+      add(:amt, :string, null: false)
+      # The same reason
+      add(:out, :string, null: false)
+      add(:rate, :float)
+      add(:evid, :integer)
+      add(:cmt, :integer)
+      add(:ss, :integer)
+      add(:addl, :integer)
+      add(:ii, :float)
+      add(:cov, :map)
+
+      timestamps()
+    end
+  end
+end
