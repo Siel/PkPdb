d23d813ba37ac124ede45b5fecbbc5753f86e02f
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-04-20 18:16:32 -0700

    #2 new_dataset form working

--- apps/web/lib/web/controllers/dataset_controller.ex
+++ apps/web/lib/web/controllers/dataset_controller.ex
@@ -1,12 +1,31 @@
 defmodule Web.DatasetController do
   use Web, :controller
+  alias Core.Dataset
 
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
-  def create(conn, %{"dataset" => dataset}) do
-    Logger.info(inspect(dataset))
+  def create(conn, %{"dataset" => dataset = %{"file" => file, "format" => format}})
+      when format in ["nonmem", "pmetrics"] do
+    case Dataset.init()
+         |> Dataset.update_attr!(%{
+           type: format,
+           name: dataset["name"],
+           description: dataset["description_text"],
+           citation: dataset["citation_text"],
+           share: "free"
+         })
+         |> Dataset.parse_events!(File.read!(file.path))
+         |> Dataset.save!() do
+      {:ok, dataset} ->
+        dataset
+
+      {:error, error} ->
+        error
+    end
+    |> IO.inspect()
+
     render(conn, "new.html")
   end
 end
