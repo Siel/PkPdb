34322351767d26c69a89db446940746b8859b6a0
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-04-29 13:21:42 -0700

    Switched to UUID

--- apps/core/lib/core/dataset/metadata.ex
+++ apps/core/lib/core/dataset/metadata.ex
@@ -6,7 +6,7 @@ defmodule Core.Dataset.Metadata do
   alias Core.Dataset.{Pmetrics, Nonmem}
 
   @primary_key {:id, :binary_id, autogenerate: false}
-  # @foreign_key_type :binary_id
+  @foreign_key_type :binary_id
   schema "metadata" do
     field :name, :string
     field :description, :string
