5732d6afd750493b84dcb5a5da6d26d82ddad07e
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-05-02 15:38:32 -0700

    first version nonmem parsing

--- apps/core/lib/core/dataset/nonmem/parse.ex
+++ apps/core/lib/core/dataset/nonmem/parse.ex
@@ -1,3 +1,115 @@
 defmodule Core.Dataset.Nonmem.Parse do
   @moduledoc false
+
+  #  alias(NimbleCSV.RFC4180, as: Nimble)
+  import Core.Dataset.ParseHelpers, only: [merge: 3]
+
+  def parse_events(str) do
+    with {:ok, headers, cov_headers} <- parse_headers(str),
+         {:ok, events} <- merge_events(headers, str),
+         {:ok, parsed_events} <- map_nonmem(events, cov_headers) do
+      IO.inspect(headers)
+      IO.inspect(parsed_events)
+    else
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  defp merge_events(headers, str) do
+    [_headers | events] =
+      str
+      |> String.trim_trailing()
+      |> String.split("\n")
+
+    events =
+      events
+      |> Enum.map(fn row -> String.split(row, ",") end)
+      |> Enum.map(&merge(headers, &1, fn x, y -> {x, y} end))
+      |> Enum.map(fn row ->
+        case row do
+          {:ok, val} ->
+            val
+            |> Enum.into(%{})
+
+          {:error, error} ->
+            raise("Error: Parsing error: #{error}")
+        end
+      end)
+
+    {:ok, events}
+  end
+
+  defp parse_headers(str) do
+    [headers | _events] =
+      str
+      |> String.split("\n")
+
+    headers =
+      headers
+      |> String.split(",")
+      |> Enum.map(&String.to_atom/1)
+
+    cov_headers =
+      headers
+      |> Enum.drop(10)
+
+    cond do
+      not valid_headers?(headers) ->
+        {:error, "Invalid header format"}
+
+      true ->
+        {:ok, headers, cov_headers}
+    end
+  end
+
+  defp map_nonmem(events, cov_headers) do
+    {:ok,
+     events
+     |> Enum.map(&do_map_nonmem(&1, cov_headers))}
+  end
+
+  defp do_map_nonmem(
+         %{
+           addl: addl,
+           amt: amt,
+           cmt: cmt,
+           dv: dv,
+           evid: evid,
+           ii: ii,
+           mdv: mdv,
+           rate: rate,
+           ss: ss,
+           time: time
+         } = event,
+         cov_headers
+       ) do
+    cov =
+      cov_headers
+      |> Enum.map(fn key -> {key, event[key]} end)
+
+    %{
+      addl: addl,
+      amt: amt,
+      cmt: cmt,
+      dv: dv,
+      evid: evid,
+      ii: ii,
+      mdv: mdv,
+      rate: rate,
+      ss: ss,
+      time: time,
+      cov: cov
+    }
+  end
+
+  defp valid_headers?(headers) do
+    length(
+      headers
+      |> Enum.uniq()
+      |> Enum.filter(fn x ->
+        x in [:addl, :amt, :cmt, :dv, :evid, :ii, :mdv, :rate, :ss, :time]
+      end)
+    ) == 10
+  end
 end
