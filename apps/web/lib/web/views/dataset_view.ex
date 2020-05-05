acc1bc19226e3b9417ed56e82f0c9b600d93c4cc
Julian D. Otalvaro <juliandavid347@gmail.com>
2020-05-05 10:54:53 -0700

    working on the search results

--- apps/web/lib/web/views/dataset_view.ex
+++ apps/web/lib/web/views/dataset_view.ex
@@ -5,4 +5,9 @@ defmodule Web.DatasetView do
     owner = Core.Accounts.get_user!(owner_id)
     "#{owner.name} #{owner.last_name}"
   end
+
+  def get_downloads(%Core.Dataset.Metadata{} = metadata) do
+    # TODO: change this, just query the length
+    length(Core.Dataset.get_downloads(%Core.Dataset{id: metadata.id, valid?: false}))
+  end
 end
